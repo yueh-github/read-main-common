6f229bd7205423ee2be9de128f1d529c8be96003
yuehao <83541408@qq.com>
2017-09-02 10:29:29 +0800

    frist commit

---
+++ src/main/java/com/readmain/common/entity/UserEntity.java
@@ -0,0 +1,16 @@
+package com.readmain.common.entity;
+
+import lombok.Data;
+
+/**
+ * Created by yuehao on 2017/8/27.
+ */
+
+@Data
+public class UserEntity {
+
+    private String name;
+
+    private Integer age;
+
+}
